0c5363f2f25048210a834f50728fab2c90f76cd5
Ehab Khaireldin <ehab.khaireldin@onemeter.com>
2018-03-02 11:42:05 +0100

    initial commit

---
+++ __tests__/main.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('main module', () => {
+    it('should match fileName', () => {
+        expect('this').to.equal('this');
+    });
+});
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const promisify = require('util').promisify
+const readFile = promisify(fs.readFile);
+const appendFile = promisify(fs.appendFile);
+
+app.get('/write/:text', async (req, res) => {
+    const { text } = req.params;
+    await appendFile(fileName, `${text}\n`);
+    res.send(`Adding '${text}' to ${fileName}`);
+});
+
+
+app.get('/read', async (req, res) => {
+    const text = await readFile(fileName, 'utf8');
+    res.send(text);
+});
+
+app.listen(5000, ()=> {
+    console.log('listening on port 5000');
+});
